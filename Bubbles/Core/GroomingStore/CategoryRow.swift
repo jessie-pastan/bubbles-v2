09e39edab3aae36ed39be575232ace2abfe8e053
jessie <jessie.pastan@gmail.com>
2023-12-27 14:43:09 -0500

    update firebase version

---
+++ Bubbles/Core/GroomingStore/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  Bubbles
+//
+//  Created by Jessie P on 8/19/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
---
+++ Bubbles/Helpers/Router.swift
@@ -0,0 +1,8 @@
+//
+//  Router.swift
+//  Bubbles
+//
+//  Created by Jessie P on 8/19/23.
+//
+
+import Foundation
